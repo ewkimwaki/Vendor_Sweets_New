59a6a0f7bcaac7ecfa28122855017b0dc0fadb10
ewkimwaki <e.w.kimwaki@gmail.com>
2024-04-15 17:02:24 +0300

    first commit

---
+++ server/app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+from models import db, Sweet, Vendor, VendorSweet
+from flask_migrate import Migrate
+from flask import Flask, request, make_response
+from flask_restful import Api, Resource
+import os
+
+
+
+app = Flask(
+    __name__,
+    static_url_path='',
+    static_folder='../client/build',
+    template_folder='../client/build'
+)
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DATABASE = os.environ.get(
+    "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+from routes import *
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
